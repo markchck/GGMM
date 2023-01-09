c911087e837d22f8d9d711af64bd62e277176c6a
markchck <markchck@gmail.com>
2023-01-09 20:48:34 +0900

    feat : 몽고디비연결, API통신 완료

---
+++ backend/models/theme.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+// mongoDB에 제시어를 저장할 스키마를 
+const Theme_Animal = new mongoose.Schema({
+    name: String
+  });
+
+// 제시어 모델정의
+const Animal = mongoose.model('Animal', Theme_Animal)
+module.exports = Animal
