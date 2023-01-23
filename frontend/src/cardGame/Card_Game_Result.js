2e8b277fd06b1feb4bdb8206bbfb3ff9f22f1bd8
경준 <kj0284@naver.com>
2023-01-23 22:00:01 +0900

    fix: 게임 시작 시 동시성 보완, 제시어 테마 반영

---
+++ frontend/src/cardGame/Card_Game_Result.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import useStore from '../for_game/store';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+function CardGameResult() {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
+  const { card_game_end, card_game_red, card_game_blue, cur_session} = useStore();
+
+  React.useEffect(() => {
+    setOpen(true);
+    setTimeout(() => {
+      setOpen(false);
+      const message = {
+        Total_score: card_game_end + 1,
+      };
+      cur_session &&
+        cur_session.signal({
+          type: "Total_score",
+          data: JSON.stringify(message),
+        });
+    }, 3000);
+  }, []);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {card_game_red > card_game_blue ? <>Red Team 이 이겼습니다.</> 
+          : <>Blue Team 이 이겼습니다.</>}
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default CardGameResult;
